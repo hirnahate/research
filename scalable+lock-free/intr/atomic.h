fb868a90d35c15a28a511683650a19e20bbf58c3
hirnahate <hirnadereg@gmail.com>
2025-08-25 15:23:33 -0700

    update to atomic.h

--- scalable+lock-free/intr/atomic.h
+++ scalable+lock-free/intr/atomic.h
@@ -2,7 +2,7 @@
 #define ATOMIC
 
 namespace atomic {
-
+/*
     // Add these specializations after your existing template functions
 template<>
 __host__ __device__ inline size_t add_system<size_t>(size_t* adr, size_t val) {
@@ -79,8 +79,86 @@ __host__ __device__ inline size_t CAS_system<size_t>(size_t* adr, size_t comp, s
             return result;
         #endif
     }
+*/
+    template<typename T>
+    __host__ __device__ T add_system(T* adr,T val) {
+        #ifdef __CUDA_ARCH__
+            return atomicAdd(adr,val);
+        #else
+            return __sync_fetch_and_add(adr,val);
+        #endif
+    }
 
-    
+    template<typename T>
+    __host__ __device__ T sub_system(T* adr,T val) {
+        #ifdef __CUDA_ARCH__
+            return atomicSub(adr,val);
+        #else
+            return __sync_fetch_and_sub(adr,val);
+        #endif
+    }
+
+    template<typename T>
+    __host__ __device__ T and_system(T* adr,T val) {
+        #ifdef __CUDA_ARCH__
+            return atomicAnd(adr,val);
+        #else
+            return __sync_fetch_and_and(adr,val);
+        #endif
+    }
+
+    template<typename T>
+    __host__ __device__ T or_system(T* adr,T val) {
+        #ifdef __CUDA_ARCH__
+            return atomicOr(adr,val);
+        #else
+            return __sync_fetch_and_or(adr,val);
+        #endif
+    }
+
+    template<typename T>
+    __host__ __device__ T xor_system(T* adr,T val) {
+        #ifdef __CUDA_ARCH__
+            return atomicXor_system(adr,val);
+        #else
+            return __sync_fetch_and_xor(adr,val);
+        #endif
+    }
+
+    template<typename T>
+    __host__ __device__ T min_system(T* adr,T val) {
+        #ifdef __CUDA_ARCH__
+            return atomicMin_system(adr,val);
+        #else
+            return __sync_fetch_and_min(adr,val);
+        #endif
+    }
+
+    template<typename T>
+    __host__ __device__ T exch_system(T* adr,T val) {
+        #ifdef __CUDA_ARCH__
+            return atomicExch_system(adr,val);
+        #else
+            T result;
+            __atomic_exchange(adr,&val,&result,__ATOMIC_ACQ_REL);
+            return result;
+        #endif
+    }
+
+    template<typename T>
+    __host__ __device__ T CAS_system(T* adr,T comp,T val) {
+        #ifdef __CUDA_ARCH__
+            return atomicCAS_system(adr,comp,val);
+        #else
+            bool success = false;
+            T expected = comp;
+            while ((!success) && (comp == expected)) {
+                success = __atomic_compare_exchange(adr,&expected,&val,false,__ATOMIC_ACQ_REL,__ATOMIC_ACQUIRE);
+            }
+            return expected;
+        #endif
+    }
+
     template<typename T>
     __host__ __device__ inline void store_relaxed(T* adr, T val) {
         #ifdef __CUDA_ARCH__
